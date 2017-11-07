676394a8218f7760f981a6a9d661ec0abbf5a53f
yukimurazero <loydgp02@sina.com>
2017-11-07 17:20:56 +0800

    add brance testa

---
+++ mymvnb/src/main/java/com/lloyd/mymvnb/App.java
@@ -0,0 +1,13 @@
+package com.lloyd.mymvnb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
